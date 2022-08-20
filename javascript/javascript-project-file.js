e07bd7ea619fc42bcd35837abe6b6e3eb8f8eba1
Gabriel Moraes <109491337+Gabriel-Moraes-CAD@users.noreply.github.com>
2022-08-20 12:15:29 -0400

    Redesigned Website/Portfolio
    A redesigned version of my portfolio

---
+++ javascript/javascript-project-file.js
@@ -0,0 +1,98 @@
+//Passenger Counter App code//
+
+//document.getElementById("count-el").innerText = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+    console.log(count)
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+//Basketball Scoreboard App code//
+
+let homeScoreDisplay = document.getElementById('homeScore')
+let guestScoreDisplay = document.getElementById('guestScore')
+let countH = 0
+let countG = 0
+//Home display functions//
+function hPlus1() {
+    countH += 1
+    homeScore.textContent = countH
+}
+function hPlus2() {
+     countH += 2
+    homeScore.textContent = countH
+}
+function hPlus3() {
+     countH += 3
+    homeScore.textContent = countH
+}
+//Guest display functions//
+function gPlus1() {
+     countG += 1
+    guestScore.textContent = countG
+}
+function gPlus2() {
+      countG += 2
+    guestScore.textContent = countG
+}
+function gPlus3() {
+      countG += 3
+    guestScore.textContent = countG
+}
+
+//Periods display functions//
+
+let periodsDiplay = document.getElementById('periods')
+
+function period1(){
+    periodsDiplay.textContent = 1
+}
+function period2(){
+    periodsDiplay.textContent = 2
+
+}
+function period3(){
+    periodsDiplay.textContent = 3
+
+}
+function period4(){
+    periodsDiplay.textContent = 4
+
+}
+
+//Timer display functions//
+
+const startingMinutes = 12;
+let time = startingMinutes * 60;
+const countdownEl = document.getElementById('timer');
+
+function updatedCountdown(){
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
+
+function timerStart(){
+    interval = setInterval(updatedCountdown, 1000);
+}
+
+function timerPause(){
+    clearInterval(interval);
+    interval = null;
+}
